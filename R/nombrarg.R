c0038e28aad23e9c4fc5556e0cda5f28b9c61cab
Daniela Laura Parada <profedanielaparada@gmail.com>
2023-08-26 01:40:22 -0300

    limpio el dataset de nombres

--- R/nombrarg.R
+++ R/nombrarg.R
@@ -2,9 +2,9 @@
 #'
 #' Datos del histórico de nombres registrados ante el Registro Nacional de las Personas en Argentina desde 1922 y hasta 2015 agrupado por nombre y año.
 #'
-#' @format Un data frame con 9761609 filas y 3 variables:
+#' @format Un data frame con 9064354 filas y 3 variables:
 #' \describe{
-#'   \item{nombre}{nombre registrado}
+#'   \item{nombre}{nombre registrado, en mayúsculas y sin tilde}
 #'   \item{cantidad}{cantidad de nombres registrados en el año}
 #'   \item{anio}{año de registro}
 #' }
