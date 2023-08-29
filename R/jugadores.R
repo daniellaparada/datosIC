ab9e6c0434837e9d9bb3fc51bd7e15fee1e846eb
Daniela Laura Parada <profedanielaparada@gmail.com>
2023-08-29 03:48:52 -0300

    agrego dataset de pablo

---
+++ R/jugadores.R
@@ -0,0 +1,19 @@
+#' Datos de jugadores de primera división
+#'
+#' Datos de jugadores de fútbol de primera división de Argentina al 2023.
+#'
+#' @format Un data frame con 1105 filas y 10 variables:
+#' \describe{
+#'   \item{Equipo}{equipo en el que juega}
+#'   \item{Puesto}{posición en la que juega}
+#'   \item{Jugador}{nombre del jugador}
+#'   \item{Edad}{edad del jugador al 2023}
+#'   \item{Nacimiento}{fecha de nacimiento del jugador, en el formato día-mes-año}
+#'   \item{Altura}{altura (cm) del jugador}
+#'   \item{Ciudad}{ciudad de nacimiento del jugador}
+#'   \item{Pais}{país de nacimiento del jugador}
+#'   \item{Mes}{mes de nacimiento del jugador}
+#'   \item{Anio}{año de nacimiento del jugador}
+#' }
+#' @source \url{https://daniellaparada.github.io/IC-datasets-docencia/otros-datasets.html}
+"jugadores"
