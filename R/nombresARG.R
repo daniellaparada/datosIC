07715ffcfcb54c4dae91fa1b9d77f9ef22588532
Daniela Laura Parada <profedanielaparada@gmail.com>
2023-09-05 02:56:14 -0300

    agrego info guaguas en descripción

--- R/nombresARG.R
+++ R/nombresARG.R
@@ -1,6 +1,6 @@
 #' Datos históricos de nombres en Argentina
 #'
-#' Datos del histórico de nombres registrados ante el Registro Nacional de las Personas en Argentina desde 1922 y hasta 2015, agrupado por nombre y año, para nombres de más de 100 ocurrencias anuales. Se preprocesó la base de [datos.gob.ar](https://datos.gob.ar/dataset/otros-nombres-personas-fisicas/archivo/otros_2.1) excluyendo, entre otros, casos de múltiples nombres (especialmente, aquellos muy antiguos).
+#' Datos del histórico de nombres registrados ante el Registro Nacional de las Personas en Argentina desde 1922 y hasta 2015, agrupado por nombre y año, para nombres de más de 100 ocurrencias anuales. Se preprocesó la base de [datos.gob.ar](https://datos.gob.ar/dataset/otros-nombres-personas-fisicas/archivo/otros_2.1) excluyendo, entre otros, casos de múltiples nombres (especialmente, aquellos muy antiguos). La librería [guaguas](https://github.com/rivaquiroga/guaguas) contiene un dataset similar, pero para el caso chileno.
 #'
 #' @format Un data frame con 76803 filas y 3 variables:
 #' \describe{
